7cf60fc046f12dc6c2d86ab0418305a7683277cb
kimYeonsoo <yeonsoo9846@gmail.com>
2022-05-12 11:21:11 +0900

    게시판 list에서 내가 쓴 글만 sorting

--- todolist-ts/src/components/sub/board/list.jsx
+++ todolist-ts/src/components/sub/board/list.jsx
@@ -1,21 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 
 function List({post,handleClickBoard}) {
+    const currentUser = useSelector(store => store.memberReducer.member);
+    const [modeMypost,setModeMyPost] = useState(false);
+
     return (
-        <ul className="boardList">
+        <>
+            <ul className="boardList">
+                {
+                    post.map((item,idx) => {
+                        if(!modeMypost) {
+                            return (
+                                <li key={item._id} onClick={() => {handleClickBoard(idx)}}>
+                                    <strong>제목 : {item.title}</strong>
+                                    {
+                                        item.writer && <p>작성자 : {item.writer}</p>
+                                    }
+                                </li>
+                            )
+                        } else if(currentUser.mail == item.writer) {
+                            return (
+                                <li key={item._id} onClick={() => {handleClickBoard(idx)}}>
+                                    <strong>제목 : {item.title}</strong>
+                                    {
+                                        item.writer && <p>작성자 : {item.writer}</p>
+                                    }
+                                </li>
+                            )
+                        }
+                    })
+                }
+            </ul>
             {
-                post.map((item,idx) => {
-                    return (
-                        <li key={item._id} onClick={() => {handleClickBoard(idx)}}>
-                            <strong>제목 : {item.title}</strong>
-                            {
-                                item.writer && <p>작성자 : {item.writer}</p>
-                            }
-                        </li>
-                    )
-                })
+                !modeMypost 
+                ? <button onClick={() => {setModeMyPost(true)}}>내가쓴 글</button>
+                : <button onClick={() => {setModeMyPost(false)}}>전체 글</button>
             }
-        </ul>
+        </>
     );
 }
 
